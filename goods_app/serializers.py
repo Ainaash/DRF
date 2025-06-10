e179efc124e2c5ec97dcb4e937623e808b3a613a
Ainash <aakmatova179@gmail.com>
2025-06-10 17:38:24 +0600

    izmena

--- goods_app/serializers.py
+++ goods_app/serializers.py
@@ -7,6 +7,10 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = '__all__'
 
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -14,7 +18,32 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
 
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class AttributeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class ProductAttributeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
 class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class ReviewImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class QuestionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
--- goods_app/views.py
+++ goods_app/views.py
@@ -1,78 +1,26 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from django_filters.views import FilterView
-from filters_app.filters import ProductFilter
-from .models import Product, Category, ProductAttribute, Brand, Questions, Review, ReviewImage
+
+from rest_framework import viewsets
+from goods_app.models  import Product, Category,Brand,Review
+from goods_app.serializers import CategorySerializer, ProductSerilizer, ReviewSerializer
+
+
+class ProductView(viewsets.ModelViewSet):
+   queryset=Product.objects.all()
+   serializer_class=ProductSerilizer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+   queryset=Category.objects.all()
+   serializer_class=ProductSerilizer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+   queryset=Review.objects.all()
+   serializer_class=ReviewSerilizer
+
+class BrandReviewSet(viewsets.ModelViewSet):
+   queryset=Brand.objects.all()
+   serializer_class=ProductSerilizer
 
 
 
-class ProductListView(FilterView):
-    model = Product
-    template_name = 'goods_app/products.html'
-    context_object_name = 'products'
-    paginate_by = 32
-    filterset_class = ProductFilter
-
-    def get_queryset(self):
-        queryset = super().get_queryset().select_related('category', 'brand').prefetch_related('images')
-
-        category_slug = self.kwargs.get('category_slug')
-        brand_slug = self.kwargs.get('brand_slug')
-
-        if category_slug:
-            queryset = queryset.filter(category__slug=category_slug)
-        if brand_slug:
-            queryset = queryset.filter(brand__slug=brand_slug)
-
-        return queryset
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
-        context['brands'] = Brand.objects.all()
-
-        category_slug = self.kwargs.get('category_slug')
-        brand_slug = self.kwargs.get('brand_slug')
-
-        if category_slug:
-            context['current_category'] = Category.objects.get(slug=category_slug)
-        if brand_slug:
-            context['current_brand'] = Brand.objects.get(slug=brand_slug)
-
-        return context
 
 
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = 'goods_app/product.html'
-    context_object_name = 'product'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # Только атрибуты текущего продукта
-        context['attributes'] = ProductAttribute.objects.filter(product=self.object)
-        # Отзывы текущего продукта
-        reviews = Review.objects.filter(product=self.object).prefetch_related('images')
-        context['reviews'] = reviews
-        # Отдельный QuerySet для изображений
-        context['review_images'] = ReviewImage.objects.filter(review__in=reviews)
-        return context
-
-class QuestionsListView(ListView):
-    model = Questions
-    template_name = 'goods_app/questions.html'
-    context_object_name = 'questions'
-
-
-class QuestionsDetailView(DetailView):
-    model = Questions
-    template_name = 'goods_app/question.html'
-    context_object_name = 'question'
-
-
-class RegisterView:
-    pass
-
-
-class ProductViewSet:
-    pass
